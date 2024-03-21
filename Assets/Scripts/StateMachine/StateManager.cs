b2c6e3ba7b42e619b79100c2e832d13afcae9817
Cherry Bomb <110393598+Ch3rryB0m@users.noreply.github.com>
2024-03-21 11:20:29 +0200

    test

--- Assets/Scripts/StateMachine/StateManager.cs
+++ Assets/Scripts/StateMachine/StateManager.cs
@@ -6,50 +6,50 @@ using UnityEngine;
 
 public abstract class StateManager<EState> : MonoBehaviour  where EState : Enum
 {
-    protected Dictionary<EState , State<EState>> States = new Dictionary<EState, State<EState>> ();
+//    protected Dictionary<EState , State<EState>> States = new Dictionary<EState, State<EState>> ();
 
-    protected State<EState> CurrentState;
+//    protected State<EState> CurrentState;
 
-    protected bool IsTransitioningState = false;
+//    protected bool IsTransitioningState = false;
 
-    void Start()
-    {
-        CurrentState.EnterState();
-    }
+//    void Start()
+//    {
+//        CurrentState.EnterState();
+//    }
 
-    void Update()
-    {
-        EState nextStateKey =CurrentState.GetNextState();
-        if (!IsTransitioningState && nextStateKey.Equals(CurrentState.StateKey)) 
-        {
-            CurrentState.UpdateState();
-        }
-        else if(!IsTransitioningState) 
-        {
-            TransitionToState(nextStateKey);
-        }
-        CurrentState.UpdateState();
-    }
-    public void TransitionToState(EState statekey)
-    {
-        IsTransitioningState = true;
-        CurrentState.ExitState();
-        CurrentState = States[statekey];
-        CurrentState.EnterState();
-        IsTransitioningState = false;
-    }
+//    void Update()
+//    {
+//        EState nextStateKey =CurrentState.GetNextState();
+//        if (!IsTransitioningState && nextStateKey.Equals(CurrentState.StateKey)) 
+//        {
+//            CurrentState.UpdateState();
+//        }
+//        else if(!IsTransitioningState) 
+//        {
+//            TransitionToState(nextStateKey);
+//        }
+//        CurrentState.UpdateState();
+//    }
+//    public void TransitionToState(EState statekey)
+//    {
+//        IsTransitioningState = true;
+//        CurrentState.ExitState();
+//        CurrentState = States[statekey];
+//        CurrentState.EnterState();
+//        IsTransitioningState = false;
+//    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        CurrentState.OnTriggerEnter(other);
-    }
+//    void OnTriggerEnter(Collider other)
+//    {
+//        CurrentState.OnTriggerEnter(other);
+//    }
 
-    void OnTriggerStay(Collider other)
-    {
-        CurrentState.OnTriggerStay(other);
-    }
-    void OnTriggerExit(Collider other)
-    {
-        CurrentState.OnTriggerExit(other);
-    }
+//    void OnTriggerStay(Collider other)
+//    {
+//        CurrentState.OnTriggerStay(other);
+//    }
+//    void OnTriggerExit(Collider other)
+//    {
+//        CurrentState.OnTriggerExit(other);
+//    }
 }
